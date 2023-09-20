51e4dd463a43a6c4a062b426f460af9b1eddeebe
Tony Nguyen <tonytnguyenn@gmail.com>
2023-09-20 15:10:07 -0500

    hereeee

---
+++ hw3/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#module load openmpi
+mpirun -np 1 ./heat_2d_loc_blk_MPI_gnu 10 100 S
+sleep 1
+mpirun -np 2 ./heat_2d_loc_blk_MPI_gnu 10 100 S
+sleep 1
+mpirun -np 3 ./heat_2d_loc_blk_MPI_gnu 10 100 S
+sleep 1
+mpirun -np 4 ./heat_2d_loc_blk_MPI_gnu 10 100 S
+sleep 1
+# execute a 10000 x 10000 point 2d-heat transfer problem
+# for 5 iternations and suppress its output
+
